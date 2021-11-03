07b2ad6a612cdf8fbdaf3e4fab0de9de602350a6
maspangl <mattspangler@protonmail.com>
2021-11-02 21:17:38 -0400

    Added license and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Qt Cisco Automation Tool',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='GPL-3',
+    author='matthewspangler',
+    author_email='mattspangler@protonmail.com',
+    description='Qt tool for running NAPALM scripts on saved device sessions.'
+)
